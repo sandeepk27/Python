35b87e784ae8aeb28817cca99a1e054b74d0b3e5
sandeep <nanisandeep876@gmail.com>
2024-12-06 21:13:59 +0530

    variables

--- 02-variables/assignment-convert-fahrenheit-temperature-to-celsius.py
+++ 02-variables/assignment-convert-fahrenheit-temperature-to-celsius.py
@@ -1,4 +1,4 @@
 user_value = input("Enter a temperature in Fahrenheit to convert to Celsius ")
-fahr_temp = float(user_value)
-cels_temp = (fahr_temp - 32) * (5/9)
-print(fahr_temp, "in Celsius is", cels_temp)
+fahrenheit_temp = float(user_value)
+celsius_temp = (fahrenheit_temp - 32) * (5/9)
+print(fahrenheit_temp, "in Celsius is", celsius_temp)
