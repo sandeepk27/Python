e37495ef6c890b4f93d128dad34b84854c7d93b5
sandeep <nanisandeep876@gmail.com>
2024-12-11 00:31:39 +0530

    functions

---
+++ 03-Functions/default-argument-values.py
@@ -0,0 +1,13 @@
+def add(a = 0, b = 0):
+    return a + b
+
+def subtract(c=0,d=0):
+    return c-d
+
+print(add(7, 8))
+print(add(10))
+print(add())
+
+
+print(subtract(121,19))
+print(subtract(25,76))
---
+++ 03-Functions/intro-to-functions.py
@@ -0,0 +1,9 @@
+# function in python
+
+def output_text():
+    print("Welcome to the program!")
+    print("It's nice to meet you")
+    print("Have an excellent day programming!")
+
+output_text()
+output_text()
