fec334b53021b372a4587b23b39c491fc4018745
sandeep <nanisandeep876@gmail.com>
2024-12-13 23:23:22 +0530

    string-methods

---
+++ 05-String-Methods/formatted-string-literals.py
@@ -0,0 +1,12 @@
+# formatting strings
+name = "Bobby"
+adjective = "green"
+noun = "alien"
+
+mad_libs = f"{name} laughed at the {adjective} {noun}."
+print(mad_libs)
+
+mad_libs = F"{name} laughed at the {adjective} {noun}."
+print(mad_libs)
+
+print(f"2 + 2 is {2 + 2}")
---
+++ 05-String-Methods/the-replace-method.py
@@ -0,0 +1,7 @@
+# replace method
+phone_number = "555 555 1234"
+print(phone_number.replace(" ", "-"))
+print(phone_number.replace("5", "9"))
+print(phone_number)
+
+phone_number = phone_number.replace(" ", "-")
